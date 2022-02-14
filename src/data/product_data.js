64450bbb329805b28d78a3605b7d0d3b655ad840
VassoD.~ <vassodor@gmail.com>
2022-02-14 13:16:02 +0100

    1

---
+++ src/data/product_data.js
@@ -0,0 +1,75 @@
+const product_card = [
+    {
+        id: 1,
+        product_name: "Xiaomi Poco X3 Pro",
+        description: "8GB/256GB",
+        price: 250,
+        currency: "€",
+        thumb: "./images/1.png"
+    },
+    {
+        id: 2,
+        product_name: "Xiaomi Redmi 9A",
+        description: "2GB/32GB",
+        price: 90,
+        currency: "€",
+        thumb: "./images/2.png"
+    },
+    {
+        id: 3,
+        product_name: "Rayes X30",
+        description: "Hands-free, Hads-on Mushc Experience",
+        price: 350,
+        currency: "€",
+        thumb: "./images/3.png"
+    },
+    {
+        id: 4,
+        product_name: "Rayes Alpha",
+        description: "Hands-free, Hads-on Mushc Experience",
+        price: 350,
+        currency: "€",
+        thumb: "./images/4.png"
+    },
+    {
+        id: 5,
+        product_name: "Rayes Z30",
+        description: "Hands-free, Hads-on Mushc Experience",
+        price: 250,
+        currency: "€",
+        thumb: "./images/5.png"
+    },
+    {
+        id: 6,
+        product_name: "Rayes X30",
+        description: "Hands-free, Hads-on Mushc Experience",
+        price: 350,
+        currency: "€",
+        thumb: "./images/6.png"
+    },
+    {
+        id: 7,
+        product_name: "Rayes Alpha",
+        description: "Hands-free, Hads-on Mushc Experience",
+        price: 350,
+        currency: "€",
+        thumb: "./images/7.png"
+    },
+    {
+        id: 8,
+        product_name: "Rayes Z30",
+        description: "Hands-free, Hads-on Mushc Experience",
+        price: 250,
+        currency: "€",
+        thumb: "./images/8.png"
+    },
+    {
+        id: 9,
+        product_name: "Rayes X30",
+        description: "Hands-free, Hads-on Mushc Experience",
+        price: 350,
+        currency: "€",
+        thumb: "./images/9.png"
+    }
+]
+export default product_card;
