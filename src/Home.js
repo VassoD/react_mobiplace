bdc347695a56d43c0572c6d940cfb90f44c9c865
VassoD.~ <vassodor@gmail.com>
2022-02-28 16:28:15 +0100

    draft button

--- src/Home.js
+++ src/Home.js
@@ -6,11 +6,12 @@ function Home() {
       <div className="intro">
         <h1>
           Welcome to Mobiplace, the place where you can see reviews for any
-          mobile of your choice on <a href="https://www.youtube.com" target="_blank" rel='noreferrer'>Youtube</a>.
+          mobile of your choice on <a href="https://www.youtube.com" target="_blank" rel='noreferrer'>Youtube</a>. 
         </h1>
+        <a href="./products">Check out the mobiles!</a>
         <img src="./images/figure.png" alt="figure"></img>
       </div>
-            <div className="intro">
+            <div className="intro2">
     <img src="./images/youtubephone.png" alt="youtubephone"></img>
       <h3>You already have the mobile listed? Press the like button if your experience was good with it or dislike if you definetely not recommend it.<span class="goto"> Go to <a href="./products">products</a> and find the mobiles.</span></h3>
     </div>
